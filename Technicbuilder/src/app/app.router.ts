64ad8cd1be2ee84fc43bf4bb95b795c6c2a668cc
SaiKishoret99 <saikishoret99@gmail.com>
2017-10-19 11:37:46 +0530

    bootstrap, router file home added

---
+++ Technicbuilder/src/app/app.router.ts
@@ -0,0 +1,25 @@
+import {
+    Routes,
+    RouterModule
+} from "@angular/router";
+import { NgModule } from "@angular/core";
+
+import { HomePage } from "./pages/home/home.page";
+
+export const allComponents = [
+    HomePage
+];
+
+export const mainRouting: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomePage}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(mainRouting)],
+    exports: [ RouterModule ]
+})
+export class MainRoutingModule {
+
+}
+
